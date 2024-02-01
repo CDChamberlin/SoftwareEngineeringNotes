7f87b59efe5a6a8a8a64a418e3de3686b1b88178
Collin <cdchamberlin.rva@gmail.com>
2024-01-31 22:02:15 -0500

    Classwork for Jan 31

---
+++ mysqldb-app/controllers/userController.js
@@ -0,0 +1,52 @@
+"use strict";
+const Models = require("../models");
+// finds all users in DB, then sends array as response
+const getUsers = (res) => {
+  Models.User.findAll({})
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+// uses JSON from request body to create new user in DB
+const createUser = (data, res) => {
+  Models.User.create(data)
+    .then((data) => {
+      res.status(201).send({ result: 201, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ result: 500, error: err.message });
+    });
+};
+// uses JSON from request body to update user ID from params
+const updateUser = (req, res) => {
+  Models.User.update(req.body, { where: { id: req.params.id } })
+    .then((data) => {
+      res.status(204).send({ result: 204, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ result: 500, error: err.message });
+    });
+};
+// deletes user matching ID from params
+const deleteUser = (req, res) => {
+  Models.User.destroy({ where: { id: req.params.id } })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ result: 500, error: err.message });
+    });
+};
+module.exports = {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
---
+++ mysqldb-app/models/index.js
@@ -0,0 +1,8 @@
+'use strict'
+const User = require('./user');
+
+async function init(){
+    await User.sync()
+}
+init()
+module.exports = {User}
